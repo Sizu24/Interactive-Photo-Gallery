c119d747819293934ff8c2277f80c294e89f74c4
Shahetul Chisty <37130013+Sizu24@users.noreply.github.com>
2020-02-07 23:52:30 -0800

    Layout position complete, thumbnails, links to images, responsive

---
+++ js/script.js
@@ -0,0 +1,6 @@
+
+  let list = document.getElementsByClassName('thumbnails').firstChild;
+  let images = '<a href="photo_gallery_v4/photos/01.jpg"></a>\
+  <img src="photo_gallery_v4/photos/thumbnails/04.jpg" alt="thumbnail image of landscape">';
+
+  console.log(list);
